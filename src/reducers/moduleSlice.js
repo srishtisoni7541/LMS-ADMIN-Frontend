dd726de43f537fc792ce6b8b7d05323784d5f9d4
srishti soni <srishtisoni7541@gmail.com>
2025-09-22 02:25:40 +0530

    project completed

--- src/reducers/moduleSlice.js
+++ src/reducers/moduleSlice.js
@@ -1,53 +1,5 @@
-// import { createSlice } from "@reduxjs/toolkit";
 
 import { createSlice } from "@reduxjs/toolkit";
-
-// const initialState = {
-//   list: [],
-//   loading: false,
-//   error: null,
-// };
-
-// const moduleSlice = createSlice({
-//   name: "modules",
-//   initialState,
-//   reducers: {
-//     setModules(state, action) {
-//       state.list = action.payload;
-//       state.loading = false;
-//       state.error = null;
-//     },
-//     addModule(state, action) {
-//       state.list.push(action.payload);
-//       state.loading = false;
-//       state.error = null;
-//     },
-//     updateModule(state, action) {
-//       const updatedModule = action.payload;
-//       state.list = state.list.map((m) =>
-//         m._id === updatedModule._id ? updatedModule : m
-//       );
-//       state.loading = false;
-//       state.error = null;
-//     },
-//     setLoading(state, action) {
-//       state.loading = action.payload;
-//     },
-//     setError(state, action) {
-//       state.error = action.payload;
-//       state.loading = false;
-//     },
-//     clearModules(state) {
-//       state.list = [];
-//       state.loading = false;
-//       state.error = null;
-//     },
-//   },
-// });
-
-// export const { setModules, addModule,updateModule, setLoading, setError, clearModules } =
-//   moduleSlice.actions;
-// export default moduleSlice.reducer;
 
 
  const initialState = {
